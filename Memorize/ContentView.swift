85c7a41ec4136a17d7a23de796aea2e009d5b821
michaellanteri <michael.lanteri05@gmail.com>
2024-08-03 11:09:39 -0400

    Lecture 2 Arrayified the cards with ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Array is a generic type, so you can have anything in it
+    //We are specifying it is of String elements
+    //Can also do [String] instead of Array<String> (preferred way)
+    let emojis: Array<String> = ["👻","🎃","🕷️","😈"]
+    
     //Body is of type View because its made up of many things (text, images, etc.)
     var body: some View {
         HStack {
-            //Building views out of other views
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            //Can't do for loops in views, but can use ForEach
+            //index is an argument of the closure
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .padding()
         .foregroundColor(.orange)
@@ -23,6 +28,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     //Vars in views are IMMUTABLE after initalizing, can't change isFaceUp directly
     //@State is only for small temporary things--game logic should not be done in a view
     //@State is a pointer, so isFaceUp can point to different things while being immutable
@@ -35,7 +41,7 @@ struct CardView: View {
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("🥳").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
